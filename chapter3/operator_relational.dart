94fb9da8c1e2025f523342c6d6b362a7308dab16
quadrant26 <quadrant26@163.com>
2019-05-17 18:20:06 +0800

    关系运算符

---
+++ chapter3/operator_relational.dart
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+void main (){
+  int a = 3;
+  int b = 5;
+
+  print(a == b);
+  print(a != b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+
+  String strA = "123";
+  String strB = "321";
+  print(strA == strB);
+
+}
