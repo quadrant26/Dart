4cba309f47f84fba9cd7d60a3f9e07ec57b81003
quadrant26 <quadrant26@163.com>
2019-05-17 17:37:13 +0800

    data type for number

---
+++ chapter2/data-type-number.dart
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+void main (){
+  num a = 10;
+  print(a);
+
+  int b = 20;
+  print(b);
+
+  double c = 10.4;
+  print(c);
+
+  print(0.0 / 0.0);
+  print(a ~/ b);
+  print(a % b);
+
+  print(b.isEven);
+  print(b.isOdd);
+  
+}
