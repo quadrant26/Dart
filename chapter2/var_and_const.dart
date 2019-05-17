cde69889b38b5c5e9cfd74ec0091177d86073242
quadrant26 <quadrant26@163.com>
2019-05-17 17:37:29 +0800

    var and const

---
+++ chapter2/var_and_const.dart
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+void main (){
+  var a;
+  print(a);
+
+  a= 10;
+  print(a);
+
+  a = "hello, dart";
+  print(a);
+
+  var b = 20;
+  print(b);
+
+  final c = 20; // final 只能声明一次
+  // c = 50;  
+  print(c);
+
+
+  const d = 20; // 常量不能被修改
+  print(d);
+  // d = 50; // 常量不能被修改
+}
