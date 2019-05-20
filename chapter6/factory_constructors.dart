bcd26e9a4e193794599751dc30fc87b751dafcbd
quadrant26 <quadrant26@163.com>
2019-05-20 14:30:48 +0800

    工厂构造方法

---
+++ chapter6/factory_constructors.dart
@@ -0,0 +1,30 @@
+void main (){
+  
+}
+
+class Logger {
+  final String name;
+  static final Map<String, Logger> _cache = <String, Logger>{};
+
+  /*
+  factory Logger(String name){
+    return Logger._internal("Dart");
+  }
+  */
+
+  factory Logger(String name){
+    if (_cache.containsKey(name)){
+      return _cache[name];
+    }else{
+      final logger = Logger._internal(name);
+      _cache[name] = logger;
+      return logger;
+    }
+  }
+
+  Logger._internal(this.name);
+
+  void log(String msg){
+    print(msg);
+  }
+}
