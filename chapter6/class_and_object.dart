e7622db6f697e25ca0c791ba6024f1174430c796
quadrant26 <quadrant26@163.com>
2019-05-20 14:11:05 +0800

    类与对象

---
+++ chapter6/class_and_object.dart
@@ -0,0 +1,13 @@
+import 'person.dart'
+
+void main (){
+  var person = new Person();
+  person.name = "Tom";
+  person.age = 20;
+
+  print(person.name);
+  print(person.address);
+  
+  person.work();
+
+}
